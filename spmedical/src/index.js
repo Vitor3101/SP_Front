10b0963cedf43f5397eacba1ea62d5dbf1924cba
Vitor <45999057809>
2021-11-17 17:26:59 -0300

    Att

--- spmedical/src/index.js
+++ spmedical/src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Home';
 import reportWebVitals from './reportWebVitals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    Route,
+    BrowserRouter as Router,
+    Redirect,
+    Switch,
+} from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const routing = ( <
+    Router >
+    <
+    div >
+    <
+    switch >
+
+    <
+    /switch> <
+    /div>
+
+
+    <
+    /Router>
+)
+
+
+ReactDOM.render(document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
